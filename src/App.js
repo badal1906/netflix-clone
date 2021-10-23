f861a6a27e9a42e9e8bebad1642f7d6449ee84ed
badal <badal1906@gmail.com>
2021-10-23 19:09:32 +0530

    add nav and trailers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import './App.css';
-import requests from './request';
-import Row from './components/Row';
-
+import "./App.css";
+import requests from "./request";
+import Row from "./components/Row";
+import Banner from "./components/Banner";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        isLargeRow
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,14 +1,19 @@
-import React, { useState, useEffect } from "react";
-import axios from "../axios";
-import "./Row.css"
-const base_url = "http://image.tmdb.org/t/p/original/";
+import React, { useState, useEffect } from 'react';
+import axios from '../axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+import "./Row.css";
 
-function Row({ title, fetchUrl }) {
+const base_url = "https://image.tmdb.org/t/p/original"
+
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-  //a snippet of code which runs of based on specific conditions
   useEffect(() => {
+
     async function fetchData() {
+
       const request = await axios.get(fetchUrl);
       setMovies(request.data.results);
       return request;
@@ -16,21 +21,43 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "99%",
+    playerVars: {
+      autoplay: 0,
+    }
+  }
+
+  const handleClick = (movie) => {
+    // console.table(movie?.title)
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      movieTrailer(movie?.title || "")
+        .then(url => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+        }).catch((error) => console.log(error));
+    }
+  }
+
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
-        {/* several row__posters */}
-        {movies.map((movie) => (
-          <img
-          key={movie.id}// will only render whats necessary
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
-            alt={movie.name}
-          />
-        ))}
+        {movies.map(movie => {
+          return <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            alt={movie.name} />
+        })}
       </div>
-      {/* container-> posters */}
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
     </div>
   );
 }
